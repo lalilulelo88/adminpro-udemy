8e7eaf7b4832aa818784b06cd06c56d0822f1e34
Luis Osorio <losorio@consejodeauditoria.gob.cl>
2019-11-07 17:19:47 -0300

    Creacion de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
+
+//Modulos 
+import { PagesModule } from './pages/pages.module';
+
+//componentes
 import { AppComponent } from './app.component';
-
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
@@ -21,21 +17,16 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    Graficas1Component,
-    ProgressComponent,
-    PagesComponent,
     RegisterComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
